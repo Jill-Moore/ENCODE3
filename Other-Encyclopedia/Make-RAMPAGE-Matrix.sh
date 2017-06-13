ff04af61b667f252c61937e837f9003224783571
Jill Moore <moorej3@z003.internal.ho.wenglab.org>
2017-06-13 16:37:03 -0400

    updated rampage script

--- Other-Encyclopedia/Make-RAMPAGE-Matrix.sh
+++ Other-Encyclopedia/Make-RAMPAGE-Matrix.sh
@@ -19,19 +19,23 @@
 source ~/.bashrc
 
 tss=~/Lab/Reference/Human/Gencode19/TSS.Filtered.bed
-width=50
-
 files=/home/jm36w/Lab/Results/V4-hg19/Rampage-List.txt
-
-experiment=$(awk -F "\t" '{if (NR=='$LSB_JOBINDEX') print $1}' $files)
-dsig=$(awk -F "\t" '{if (NR=='$LSB_JOBINDEX') print $2}' $files)
-dline=$(awk -F "\t" '{if (NR=='$LSB_JOBINDEX') print $0}' $files)
+width=50
 
 mkdir -p /home/jm36w/Lab/Results/V4-hg19/rampage/$LSB_JOBID"-"$LSB_JOBINDEX
 cd /home/jm36w/Lab/Results/V4-hg19/rampage/$LSB_JOBID"-"$LSB_JOBINDEX
 
-awk -F "\t" '{printf "%s\t%.0f\t%.0f\t%s\n", $1,$2-'$width',$3+'$width',$4}' $peaks | awk '{if ($2 < 0) print $1 "\t" 0 "\t" $3 "\t" $4 ; else print $0}' | sort -u > little
+experiment=$(awk -F "\t" '{if (NR=='$LSB_JOBINDEX') print $1}' $files)
+signal=$(awk -F "\t" '{if (NR=='$LSB_JOBINDEX') print $2}' $files)
+bigwig=/project/umw_zhiping_weng/0_metadata/encode/data/$experiment/$signal.bigWig
 
-~/bin/bigWigAverageOverBed -bedOut=out2.bed /project/umw_zhiping_weng/0_metadata/encode/data/$experiment/$dsig.bigWig little out2
+
+awk '{printf "%s\t%.0f\t%.0f\t%s\n", $1,$2-'$width',$3+'$width',$4}' $tss | \
+    awk '{if ($2 < 0) print $1 "\t" 0 "\t" $3 "\t" $4 ; else print $0}' | \
+    sort -u > tmp.bed
+
+~/bin/bigWigAverageOverBed $bigwig tmp.bed out2
 
 sort -k1,1 out2 | awk 'BEGIN{print "'$dsig'"}{print $4}' > $LSB_JOBINDEX".Results"
+
+rm out2 tmp.bed
